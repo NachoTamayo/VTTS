3f326fe394c343d1e5a0597032f8504edc327164
Nacho Tamayo <igtalo@gmail.com>
2024-09-20 12:14:57 +0200

    refactor: Update formatDate function to use ISO date format

--- helpers/js-utils.ts
+++ helpers/js-utils.ts
@@ -1,7 +1,7 @@
-export function formatDate(dateString: string): string {
-  const [datePart] = dateString.split(" ");
-
-  const [year, month, day] = datePart.split("/");
-
+export function formatDate(isoDate: string): string {
+  const date = new Date(isoDate);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Los meses empiezan desde 0
+  const year = date.getFullYear();
   return `${day}/${month}/${year}`;
 }
