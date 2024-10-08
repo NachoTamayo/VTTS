e31e4b4b5f435ca636f6424a817b078c45d3d4fa
Nacho Tamayo <igtalo@gmail.com>
2024-10-08 10:14:59 +0200

    Toast from Sonner implemented

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,10 +1,12 @@
 import { Layout } from "@/components/layout/layout";
 import "@/styles/globals.css";
+import { Toaster } from "sonner";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <Layout>{children}</Layout>;
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Layout>
+      {children}
+      <Toaster />
+    </Layout>
+  );
 }
