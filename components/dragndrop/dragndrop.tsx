89b498f7b59f0a76f80a0b28434b998fbc96b77f
Nacho Tamayo <igtalo@gmail.com>
2024-10-09 23:24:41 +0200

    Drag and drop updated

--- components/dragndrop/dragndrop.tsx
+++ components/dragndrop/dragndrop.tsx
@@ -12,15 +12,18 @@ import {
 } from "@/components/icons/icons";
 import "@/components/dragndrop/dragndrop.css";
 import { Button, Spacer, Progress } from "@nextui-org/react";
+import { toast } from "sonner";
 
 const DragNdrop = ({
   onFilesSelected,
   width,
   height,
+  fileName,
 }: {
   onFilesSelected: Function;
   width: number | string;
   height: number;
+  fileName?: string;
 }) => {
   const [files, setFiles] = useState<File[]>([]);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
@@ -90,6 +93,26 @@ const DragNdrop = ({
     setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
   };
 
+  const handleGetFile = async (fileName: string) => {
+    // Realizar la solicitud HTTP para obtener el archivo
+    const response = await fetch("/documents/" + fileName);
+
+    // Comprobar si la respuesta fue exitosa
+    if (!response.ok) {
+      toast.error("Error downloading file");
+    }
+
+    // Convertir la respuesta en un Blob
+    const blob = await response.blob();
+
+    // Crear un objeto File a partir del Blob
+    const file = new File([blob], fileName, {
+      type: blob.type,
+    });
+
+    return file;
+  };
+
   const handleFileType = (file: string) => {
     if (file == "application/vnd.openxmlformats-officedocument.wordprocessingml.document") return <DocIcon />;
     else if (file.includes("jpg") || file.includes("jpeg") || file.includes("png")) return <ImgIcon />;
@@ -98,9 +121,20 @@ const DragNdrop = ({
     else if (file == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") return <XLSIcon />;
     else return <FileUnknownIcon />;
   };
+  useEffect(() => {
+    console.log(fileName);
+    if (fileName != undefined && fileName != "") {
+      handleGetFile(fileName).then((file) => {
+        setFiles((prevFiles) => [file]);
+        onFilesSelected(file);
+      });
+    } else {
+      setFiles([]);
+    }
+  }, []);
 
   useEffect(() => {
-    onFilesSelected(files);
+    onFilesSelected(files[0]);
   }, [files, onFilesSelected]);
 
   return (
@@ -108,30 +142,43 @@ const DragNdrop = ({
       <div
         className={`document-uploader ${files.length > 0 ? "upload-box active" : "upload-box"}`}
         onDrop={handleDrop}
+        onClick={handleBrowse}
         onDragOver={(event) => event.preventDefault()}>
         {files.length == 0 && (
           <>
             <div className="upload-info">
               <div className="flex flex-col text-center items-center mt-4">
-                <CloudUploadIcon width={50} height={50} color={"#52525b"} className="opacity-50 ms-5" />
-                <p className="text-xs" style={{ color: "#52525b" }}>
-                  Drag and drop your files here
-                </p>
-                <p className="text-xs" style={{ color: "#52525b" }}>
-                  Limit 1.5MB per file. Supported files: .pdf, .docx, .jpg, .png, .txt, .xlsx
-                </p>
-                <input
-                  type="file"
-                  hidden
-                  id="browse"
-                  ref={fileInputRef}
-                  onChange={handleFileChange}
-                  accept=".pdf,.docx,.jpg,.png,.txt,.xlsx"
+                <CloudUploadIcon
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    zIndex: 0,
+                    opacity: 0.05,
+                  }}
                 />
-                <Spacer y={3} />
-                <Button variant="light" color="primary" size="sm" onClick={handleBrowse}>
-                  Browse
-                </Button>
+                <div style={{ zIndex: 10 }}>
+                  <p className="text-xs" style={{ color: "#52525b" }}>
+                    Drag and drop your files here
+                  </p>
+                  <p className="text-xs" style={{ color: "#52525b" }}>
+                    Limit 1.5MB per file. Supported files: .pdf, .docx, .jpg, .png, .txt, .xlsx
+                  </p>
+                  <input
+                    type="file"
+                    hidden
+                    id="browse"
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                    accept=".pdf,.docx,.jpg,.png,.txt,.xlsx"
+                  />
+                  {/* <Spacer y={3} />
+                  <Button variant="light" color="primary" size="sm" onClick={handleBrowse}>
+                    Browse
+                  </Button> */}
+                </div>
               </div>
             </div>
           </>
@@ -142,8 +189,8 @@ const DragNdrop = ({
             <div className="flex justify-center items-center w-full h-full">
               <table className="table-auto border-separate border-spacing-2">
                 {files.map((file, index) => (
-                  <>
-                    <tr key={index}>
+                  <tbody key={index}>
+                    <tr>
                       <td>{handleFileType(file.type)}</td>
                       <td> {file.name}</td>
                       <td>
@@ -167,18 +214,18 @@ const DragNdrop = ({
                       <td></td>
                       <td></td>
                     </tr>
-                  </>
+                  </tbody>
                 ))}
               </table>
             </div>
           </div>
         )}
-
+        {/* 
         {files.length > 0 && (
           <div className="success-file">
             <p>{files.length} file(s) selected</p>
           </div>
-        )}
+        )} */}
       </div>
     </section>
   );
