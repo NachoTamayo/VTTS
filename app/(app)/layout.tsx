b5e9203447558da858b101fb684c4bcaa1357b3c
Nacho Tamayo <igtalo@gmail.com>
2024-10-10 23:02:22 +0200

    Alert System improv.

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <Layout>
       {children}
-      <Toaster />
+      <Toaster richColors closeButton />
     </Layout>
   );
 }
