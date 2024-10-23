a8166ede0808e1de7c9da774de87c1f9cba21eb1
Nacho Tamayo <igtalo@gmail.com>
2024-10-23 22:20:52 +0200

    Refactor import paths for routing in middleware.ts and i18n/request.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
+import { routing } from "./routing";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { verifyJWT } from "@/helpers/jwt-verificator";
