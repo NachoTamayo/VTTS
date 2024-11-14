689bb38549cff5653b48943410b0949a6103471f
Nacho Tamayo <igtalo@gmail.com>
2024-11-14 22:52:52 +0100

    Refactor serviceRequest route to include PUT and DELETE methods

--- app/api/v1/serviceRequest/[id]/route.ts
+++ app/api/v1/serviceRequest/[id]/route.ts
@@ -17,3 +17,41 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
     return NextResponse.json({ success: false, error: "Failed to fetch data" }, { status: 500 });
   }
 }
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+  try {
+    const { srNumber, srType, statusSr, description, externalLink, trelloLink } = await req.json();
+    console.log(srNumber, srType, statusSr, description, externalLink, trelloLink);
+    const result = await prisma.serviceRequest.update({
+      where: {
+        id: parseInt(params.id),
+      },
+      data: {
+        srNumber: srNumber,
+        trelloLink: trelloLink,
+        externalLink: externalLink,
+        description: description,
+        srType: parseInt(srType),
+        statusSr: statusSr,
+      },
+    });
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    console.error("Error updating service request", error);
+    return NextResponse.json({ success: false, error: "Failed to update data" }, { status: 500 });
+  }
+}
+
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+  try {
+    await prisma.serviceRequest.delete({
+      where: {
+        id: parseInt(params.id),
+      },
+    });
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error("Error deleting service request", error);
+    return NextResponse.json({ success: false, error: "Failed to delete data" }, { status: 500 });
+  }
+}
