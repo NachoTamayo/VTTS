5985a6ccdb804ab91840751bd1fd81f56dfa6d34
Nacho Tamayo <igtalo@gmail.com>
2024-11-16 22:39:27 +0100

    Refactor InfoAlert component to include multilang support and improve accessibility

--- components/alert/InfoAlert.tsx
+++ components/alert/InfoAlert.tsx
@@ -3,15 +3,20 @@ import { useEffect } from "react";
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
 import { InfoAlertProps } from "@/helpers/interfaces";
 import { useAlert } from "@/helpers/alert-context";
+import { useTranslations } from "next-intl";
+import { AlertCircleIcon } from "@/components/icons/Icons";
 
 export const InfoAlert: React.FC<InfoAlertProps> = (props) => {
   const { alert, showAlert, hideAlert } = useAlert();
+  const g = useTranslations("Global");
+  const t = useTranslations(alert && alert.multilang ? alert.multilang : "Global");
   let { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
   return (
     <>
       {alert && (
         <Modal
           backdrop="opaque"
+          hideCloseButton={true}
           isOpen={true} // Usa el estado `isOpen`
           onOpenChange={onOpenChange}
           motionProps={{
@@ -37,15 +42,30 @@ export const InfoAlert: React.FC<InfoAlertProps> = (props) => {
           <ModalContent>
             {(onClose) => (
               <>
-                <ModalBody>
-                  <p>{alert.message}</p>
+                <ModalHeader>
+                  <p></p>
+                </ModalHeader>
+                <ModalBody className="text-center">
+                  <AlertCircleIcon width={60} height={60} color={"#f31260"} className="mx-auto" />
+                  {alert.type === "info" ? (
+                    <h1>{g("messages.info")}</h1>
+                  ) : (
+                    <p className="text-2xl">{g("messages.delete")}</p>
+                  )}
+                  <p className="text-slate-500">{t(alert.message)}</p>
                 </ModalBody>
-                <ModalFooter>
-                  <Button size="sm" color="danger" variant="light" onPress={hideAlert}>
-                    Close
+                <ModalFooter className="justify-center">
+                  <Button size="sm" color="danger" onPress={hideAlert}>
+                    {alert.type === "info" ? g("close") : g("cancel")}
                   </Button>
-                  <Button size="sm" color="primary" onPress={hideAlert}>
-                    Action
+                  <Button
+                    size="sm"
+                    color="primary"
+                    onClick={() => {
+                      if (alert.callback && typeof alert.callback === "function") alert.callback();
+                      hideAlert();
+                    }}>
+                    {alert.type === "info" ? g("ok") : g("confirm")}
                   </Button>
                 </ModalFooter>
               </>
