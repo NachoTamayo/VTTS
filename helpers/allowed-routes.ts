2120fc7875f9b9bc02c6d6bcc48c725b305cbf4a
Nacho Tamayo <igtalo@gmail.com>
2024-11-16 22:39:49 +0100

    improve security

--- helpers/allowed-routes.ts
+++ helpers/allowed-routes.ts
@@ -1,1 +1,11 @@
-export const allowedRoutes = ["/", "/test-release", "/version", "/serviceRequest"];
+export const allowedRoutes = [
+  "/",
+  "/en",
+  "/es",
+  "/en/testRelease",
+  "/es/testRelease",
+  "/en/version",
+  "/es/version",
+  "/en/serviceRequest",
+  "/es/serviceRequest",
+];
